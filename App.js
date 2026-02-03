54efcdb99605917dc24347255f12b9987553ab58
fruitsalad266 <fruitsalad266@gmail.com>
2026-02-02 19:15:30 -0800

    minor changes

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is the Wavelength Application Test</Text>
       <StatusBar style="auto" />
     </View>
   );
