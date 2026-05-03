70659317993677976f694affb13b25b17f53d3b9
fruitsalad266 <fruitsalad266@gmail.com>
2026-05-03 01:32:58 -0700

    EAS for testflight

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -25,6 +25,8 @@ export function AuthProvider({ children }) {
       } else {
         setLoading(false);
       }
+    }).catch(() => {
+      setLoading(false);
     });
 
     const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
